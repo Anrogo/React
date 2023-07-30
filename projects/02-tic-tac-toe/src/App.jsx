902c6142b0946191db80c9a8429dde1cfbf9c746
anrogo <antoniorogo96@gmail.com>
2023-07-30 12:40:56 +0200

    actualizando proyecto

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from "react"
 import './App.css'
 import { Square } from './components/Square.jsx'
 import { TURNS } from './constants.js'
@@ -63,6 +63,10 @@ function App() {
       setWinner(false) //empate
     }
   }
+
+  useEffect(() => {
+    console.log('useEffect')
+  }, [turn, board])
 
   return (
     <main className="board">
